53295fbab388040867bad94cfb4dba8d82830165
aldoarya <arudo.saka@gmail.com>
2022-07-29 17:17:01 +0700

    feat: Initialized NextJS and TailwindCSS

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Discover Donorojo | Moro Donorojo 2022</title>
+        <meta
+          name="description"
+          content="KKN-PPM (Kuliah Kerja Nyata - Pembelajaran Pemberdayaan Masyarakat) UGM adalah salah  satu bentuk  nyata  komitmen  kerakyatan  dan kebangsaan Universitas Gadjah Mada. KKN-PPM UGM ini  merupakan  mata  kuliah  wajib  yang mempunyai  sejarah  panjang;  rintisannya  telah dimulai  sejak  tahun  1951. KKN-PPM UGM 2022 Moro Donorojo menyasar dua desa yang terdapat di Kecamatan Donorojo, Kabupaten Pacitan, yaitu Desa Sendang dan Desa Kalak. KKN-PPM UGM 2022 Moro Donorojo memiliki tema 'Peningkatan Ekonomi Desa Kalak dan Sendang yang Berkelanjutan Melalui Konsep Ekowisata' yang bertujuan untuk mengembangkan potensi pariwisata dan meningkatkan ekonomi masyarkat pada Desa Sendang dan Desa Kalak"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
